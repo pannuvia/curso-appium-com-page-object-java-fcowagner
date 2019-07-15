81286feb1447df628a6cdfe37789f52d56844721
Pannuvia Monteiro <pannuvia@hotmail.com>
2019-07-15 15:39:51 -0300

    Metodo preencherCampoTexto()

--- src/main/java/br/rs/pannuviamonteiro/appium/cttreinamento/FormularioTest.java
+++ src/main/java/br/rs/pannuviamonteiro/appium/cttreinamento/FormularioTest.java
@@ -2,31 +2,52 @@ package br.rs.pannuviamonteiro.appium.cttreinamento;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class FormularioTest {
 
-//	private AndroidDriver driver;
-
 	@Test
-	public void deveInstalarAPK() throws MalformedURLException {
+	public void preencherCampoTexto() throws MalformedURLException {
 		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 		desiredCapabilities.setCapability("platformName", "Android");
 		desiredCapabilities.setCapability("deviceName", "emulator-5554");
 		desiredCapabilities.setCapability("automationName", "UiAutomator2");
 		desiredCapabilities.setCapability(MobileCapabilityType.APP, "D:\\DBSERVER\\BACKUP PROJETOS\\eclipse-workspace\\curso-appium-java-fcowagner\\src\\main\\resources\\CTAppium-1-1.apk");
-		desiredCapabilities.setCapability("noReset", "true");
+		//desiredCapabilities.setCapability("noReset", "true");
 
 		AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
-
-
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+//		//Listar elementos da tela
+//		List<MobileElement> elementosEncontrados = driver.findElements(By.className("android.widget.TextView"));
+//		for(MobileElement elemento: elementosEncontrados) {
+//			System.out.println(elemento.getText());
+//		}
+				
+		//selecionar Formulário
+		List<MobileElement> elementosEncontrados = driver.findElements(By.className("android.widget.TextView"));
+		elementosEncontrados.get(1).click();
+		
+		
+		//escrever nome
+		MobileElement campoNome = driver.findElement(MobileBy.AccessibilityId("nome"));
+		campoNome.sendKeys("Pannuvia");
+		
+		//verificar nome preenchido
+		String nomePreenchido = campoNome.getText();
+		Assert.assertEquals("Pannuvia", nomePreenchido);
+		
 		driver.quit();
 	}
-
 }
---
+++ src/main/java/br/rs/pannuviamonteiro/appium/cttreinamento/InstalarAPKTest.java
@@ -0,0 +1,28 @@
+package br.rs.pannuviamonteiro.appium.cttreinamento;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class InstalarAPKTest {
+
+	@Test
+	public void instalarAPK() throws MalformedURLException {
+		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+		desiredCapabilities.setCapability("platformName", "Android");
+		desiredCapabilities.setCapability("deviceName", "emulator-5554");
+		desiredCapabilities.setCapability("automationName", "UiAutomator2");
+		desiredCapabilities.setCapability(MobileCapabilityType.APP, "D:\\DBSERVER\\BACKUP PROJETOS\\eclipse-workspace\\curso-appium-java-fcowagner\\src\\main\\resources\\CTAppium-1-1.apk");
+		desiredCapabilities.setCapability("noReset", "true");
+
+		AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+
+		driver.quit();
+	}
+}
