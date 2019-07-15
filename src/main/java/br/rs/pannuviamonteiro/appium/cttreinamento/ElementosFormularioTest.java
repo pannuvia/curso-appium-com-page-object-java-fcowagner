506ee892f8a2d30de2a6a7c078e4cafac94236fc
Pannuvia Monteiro <pannuvia@hotmail.com>
2019-07-15 17:10:06 -0300

    Criando classe ElementosFormulariosTest

--- src/main/java/br/rs/pannuviamonteiro/appium/cttreinamento/ElementosFormularioTest.java
+++ src/main/java/br/rs/pannuviamonteiro/appium/cttreinamento/ElementosFormularioTest.java
@@ -15,7 +15,7 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
-public class FormularioTest {
+public class ElementosFormularioTest {
 
 	@Test
 	public void preencherCampoTexto() throws MalformedURLException {
