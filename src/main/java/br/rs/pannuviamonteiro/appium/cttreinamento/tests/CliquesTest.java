9adfa0f7f395534e70e6c76e0f9b951c468d8f42
Pannuvia Monteiro <pannuvia@hotmail.com>
2019-07-23 10:15:28 -0300

    Ajustes nas classes CliquesPage e CliquesTest para acrescentar o clique duplo

--- src/main/java/br/rs/pannuviamonteiro/appium/cttreinamento/tests/CliquesTest.java
+++ src/main/java/br/rs/pannuviamonteiro/appium/cttreinamento/tests/CliquesTest.java
@@ -2,6 +2,7 @@ package br.rs.pannuviamonteiro.appium.cttreinamento.tests;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.rs.pannuviamonteiro.appium.cttreinamento.pages.CliquesPage;
@@ -17,12 +18,25 @@ public class CliquesTest extends BaseTest {
 		this.cliquesPage = new CliquesPage();
 	}
 	
+	@Before
+	public void setUp() {
+		menuPage.acessarCliques();
+	}
+	
 	@Test
 	public void deveRealizarCliqueLongo() {
-		menuPage.acessarCliques();
-		
+				
 		cliquesPage.clicarEmCliqueLongo();
 		
 		assertEquals("Clique Longo", cliquesPage.capturarTextoAposClicar());
+	}
+	
+	@Test
+	public void deveRealizarCliqueDuplo() {
+				
+		menuPage.clicarPorTexto("Clique duplo");
+		menuPage.clicarPorTexto("Clique duplo");
+		
+		assertEquals("Duplo Clique", cliquesPage.capturarTextoAposClicar());
 	}
 }
