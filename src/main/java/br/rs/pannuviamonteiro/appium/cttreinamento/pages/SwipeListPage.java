b869f26c292465c23d7c3c060d8c2ca1d29431ff
Pannuvia Monteiro <pannuvia@hotmail.com>
2019-07-25 19:27:18 -0300

    Refatoração das classes SwipeListPage e SwipeListTest, para que as mesmas estejam no padrão PageObject após refatoração das classes BasePage e BaseTest

---
+++ src/main/java/br/rs/pannuviamonteiro/appium/cttreinamento/pages/SwipeListPage.java
@@ -0,0 +1,41 @@
+package br.rs.pannuviamonteiro.appium.cttreinamento.pages;
+
+public class SwipeListPage extends BasePage {
+
+//public MobileElement elementoSwipeList(String textoSwipeList) {
+//return elementoPorXPathComTexto(textoSwipeList);
+//}
+//
+//public MobileElement elementoOpcao1(String tituloOpcao1) {
+//return elementoPorXPathComTexto(tituloOpcao1);
+//}
+//	
+//public MobileElement elementoOpcao2(String tituloOpcao2) {
+//return elementoPorXPathComTexto(tituloOpcao2);
+//}
+//	
+//public MobileElement elementoOpcao3(String tituloOpcao3) {
+//return elementoPorXPathComTexto(tituloOpcao3);
+//}
+//	
+//public MobileElement elementoOpcao4(String tituloOpcao4) {
+//return elementoPorXPathComTexto(tituloOpcao4);
+//}
+//	
+//public MobileElement elementoOpcao5(String tituloOpcao5) {
+//return elementoPorXPathComTexto(tituloOpcao5);
+//}
+//	
+//public MobileElement elementoOpcao6(String tituloOpcao6) {
+//return elementoPorXPathComTexto(tituloOpcao6);
+//}
+//	
+//public MobileElement elementoOpcao7(String tituloOpcao7) {
+//return elementoPorXPathComTexto(tituloOpcao7);
+//}
+//	
+//public MobileElement elementoOpcao8(String tituloOpcao8) {
+//return elementoPorXPathComTexto(tituloOpcao8);
+//}
+	
+}
